6abdfa9fafdbd826b32fc2b9c4180040818ae9d2
Abdullah's PC <abdullahalrahman23@gmail.com>
2023-11-14 18:40:37 +0300

    added get api

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const cors = require('cors')
+const app = express();
+require('dotenv').config()
+const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+///middle ware
+app.use(cors())
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0veicth.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        ///collection
+        const menuCollection = client.db('bisttroDb').collection('menu')
+        const reviewsCollection = client.db('bisttroDb').collection('reviews')
+
+
+
+
+
+
+        ///get all data of menu
+        app.get('/menu', async (req, res) => {
+            const result = await menuCollection.find().toArray();
+            res.send(result)
+        })
+
+        ///get all data of review
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewsCollection.find().toArray();
+            res.send(result)
+        })
+
+
+
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } catch {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Server is running')
+})
+
+
+app.listen(port, () => {
+    console.log(`Bistro restaurent is running on the port ${port}`)
+}) 
